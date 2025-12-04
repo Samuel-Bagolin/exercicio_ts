d8ef082aecfbde2ee0702964be14bacba0415c2f
Samuel Bagolin <160024298+Samuel-Bagolin@users.noreply.github.com>
2025-12-04 17:00:35 -0300

    multiplicacao.ts

---
+++ src/multiplicacao.ts
@@ -0,0 +1,11 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudar(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Exemplos de uso
+console.log(multiplicar(5, 3)); 
+console.log(saudar("Maria"));
