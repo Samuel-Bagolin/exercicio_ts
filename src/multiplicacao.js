b407cec6f07378fcd3de8b6bc127345a497a3630
Samuel Bagolin <160024298+Samuel-Bagolin@users.noreply.github.com>
2025-12-04 17:00:10 -0300

    multiplicacao.js

---
+++ src/multiplicacao.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudar(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso
+console.log(multiplicar(5, 3));
+console.log(saudar("Maria"));
